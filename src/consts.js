00e10718571c76d185a04c84ca27fc17afc1b26c
mina-gh <minamail@gmail.com>
2018-08-07 16:09:54 -0400

    installed mapbox and tested

---
+++ src/consts.js
@@ -0,0 +1,3 @@
+export const API_KEY = "pk.eyJ1IjoiYWx1ZHdpbjEiLCJhIjoiY2prazRwMXFzMW42MTNya3hnZGtoejZoMCJ9.gPGbPlk4UzVpDJyWpWYQeg";
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log("hello world");
-var x = 1;
+import {API_KEY}  from './consts.js';
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = API_KEY;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
